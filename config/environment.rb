089b65ab9a54c86fe2ee9b9471979e03d768b242
Ruslan Kenzhebekov <reke.essential@gmail.com>
2012-07-08 12:12:09 -0700

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,18 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :test
+  #config.action_mailer.smtp_settings = {
+  #    address: "smtp.gmail.com",
+  #    port: 587,
+  #    domain: "domain.of.sender.net",
+  #    authentication: "plain",
+  #    user_name: "",
+  #    password: "",
+  #    enable_starttls_auto: true
+  #}
+end
+
+# Initialize the rails application
+Depot::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '153c8c42606a04004101b4ba7217476e0b50799c4a311d69b47e54015cf54fbe485836e5557286028de5d7b25d2899fe1c8ff894c6e406ff3908cc72d711230d'
